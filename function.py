86480e238019dfe172e2e3cadca18bb3d9deab75
ChrisKim <1515748548@qq.com>
2022-04-08 13:16:23 +0800

    调整正则匹配逻辑，减少不必要的判断

--- function.py
+++ function.py
@@ -8,28 +8,30 @@ from send import send_user, send_group, send_group_at
 
 
 def keyword_user(message, uid):
-    if match(r"^\.help|--help$", message):
-        send_user(uid, func_help())
-    elif match(r"^\.moyu|--moyu$", message):
-        send_user(uid, func_moyu())
-    elif match(r"^(\.gacha|--gacha) (?P<type>[0-9]+) (?P<count>[0-9]+)$", message):
-        arg = match(r"^(\.gacha|--gacha) (?P<type>[0-9]+) (?P<count>[0-9]+)$", message).groupdict()
-        send_user(uid, func_gacha(int(arg["type"]), int(arg["count"])))
-    elif match(r"^\.gsupd|--gsupd$", message):
-        send_user(uid, gsupd())
-    elif match(r"^\.cee|--cee|\.gk|--gk$", message):
-        send_user(uid, cee())
+    if match(r"^\..*$|^--.*$", message):
+        if match(r"^\.help|--help$", message):
+            send_user(uid, func_help())
+        elif match(r"^\.moyu|--moyu$", message):
+            send_user(uid, func_moyu())
+        elif match(r"^(\.gacha|--gacha) (?P<type>[0-9]+) (?P<count>[0-9]+)$", message):
+            arg = match(r"^(\.gacha|--gacha) (?P<type>[0-9]+) (?P<count>[0-9]+)$", message).groupdict()
+            send_user(uid, func_gacha(int(arg["type"]), int(arg["count"])))
+        elif match(r"^\.gsupd|--gsupd$", message):
+            send_user(uid, gsupd())
+        elif match(r"^\.cee|--cee|\.gk|--gk$", message):
+            send_user(uid, cee())
 
 
 def keyword_group(message, uid, gid):
-    if match(r"^\.help|--help$", message):
-        send_group(gid, func_help())
-    elif match(r"^\.moyu|--moyu$", message):
-        send_group(gid, func_moyu())
-    elif match(r"^(\.gacha|--gacha) (?P<type>[0-9]+) (?P<count>[0-9]+)$", message):
-        arg = match(r"^(\.gacha|--gacha) (?P<type>[0-9]+) (?P<count>[0-9]+)$", message).groupdict()
-        send_group_at(gid, uid, func_gacha(int(arg["type"]), int(arg["count"])))
-    elif match(r"^\.gsupd|--gsupd$", message):
-        send_group(gid, gsupd())
-    elif match(r"^\.cee|--cee|\.gk|--gk$", message):
-        send_group(gid, cee())
+    if match(r"^\..*$|^--.*$", message):
+        if match(r"^\.help|--help$", message):
+            send_group(gid, func_help())
+        elif match(r"^\.moyu|--moyu$", message):
+            send_group(gid, func_moyu())
+        elif match(r"^(\.gacha|--gacha) (?P<type>[0-9]+) (?P<count>[0-9]+)$", message):
+            arg = match(r"^(\.gacha|--gacha) (?P<type>[0-9]+) (?P<count>[0-9]+)$", message).groupdict()
+            send_group_at(gid, uid, func_gacha(int(arg["type"]), int(arg["count"])))
+        elif match(r"^\.gsupd|--gsupd$", message):
+            send_group(gid, gsupd())
+        elif match(r"^\.cee|--cee|\.gk|--gk$", message):
+            send_group(gid, cee())
