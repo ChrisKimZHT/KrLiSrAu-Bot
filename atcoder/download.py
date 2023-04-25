38335b29654daca6a2fdfb56a96cdbae6dd98856
ChrisKim <1515748548@qq.com>
2023-04-25 17:37:28 +0800

    [fix]: <atcoder> 异步HTTP请求

--- atcoder/download.py
+++ atcoder/download.py
@@ -1,4 +1,4 @@
-import requests
+import aiohttp
 
 
 async def req_page() -> str:
@@ -7,5 +7,6 @@ async def req_page() -> str:
         "referer": "https://atcoder.jp/",
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
     }
-    respounce = requests.get(url, headers=headers)
-    return respounce.content.decode("UTF-8")
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url=url, headers=headers) as resp:
+            return await resp.text()
