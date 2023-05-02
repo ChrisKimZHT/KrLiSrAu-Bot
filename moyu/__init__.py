6c9d276bbe01a8cea69e21979fc1346eeb506ca4
ChrisKim <1515748548@qq.com>
2023-05-02 14:47:18 +0800

    [feat]: <moyu> 支持定时发送

--- moyu/__init__.py
+++ moyu/__init__.py
@@ -1,7 +1,7 @@
-from nonebot import on_command
+from nonebot import on_command, require, get_bot
 from nonebot.plugin import PluginMetadata
 from .moyu import mo_yu
-from .config import Config
+from .config import Config, moyu_config
 
 __plugin_meta__ = PluginMetadata(
     name="摸鱼办",
@@ -11,7 +11,7 @@ __plugin_meta__ = PluginMetadata(
     config=Config,
     extra={
         "authors": "ChrisKim",
-        "version": "1.0.4",
+        "version": "1.1.0",
         "KrLiSrAu-Bot": True,
     }
 )
@@ -23,3 +23,19 @@ moyu = on_command("moyu", aliases={"摸鱼"}, priority=1, block=True)
 async def handle_moyu():
     result = mo_yu()
     await moyu.finish(result)
+
+
+if moyu_config.klsa_moyu_schedule:
+    require("nonebot_plugin_apscheduler")
+    from nonebot_plugin_apscheduler import scheduler
+
+
+    @scheduler.scheduled_job("cron", hour=moyu_config.klsa_moyu_schedule_hour,
+                             minute=moyu_config.klsa_moyu_schedule_minute, id="moyu")
+    async def _():
+        bot = get_bot()
+        result = mo_yu()
+        for group in moyu_config.klsa_moyu_schedule_group:
+            await bot.call_api("send_group_msg", group_id=group, message=result)
+        for user in moyu_config.klsa_moyu_schedule_user:
+            await bot.call_api("send_private_msg", user_id=user, message=result)
