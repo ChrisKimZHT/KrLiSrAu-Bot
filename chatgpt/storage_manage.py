42adaa32b786c6e1b0cc2e90ee837568babea410
ChrisKim <1515748548@qq.com>
2023-05-25 13:09:18 +0800

    [feat]: <chatgpt> 本地文件标记版本号，防止不兼容

--- chatgpt/storage_manage.py
+++ chatgpt/storage_manage.py
@@ -10,6 +10,7 @@ import nonebot_plugin_localstore as store
 
 driver = get_driver()
 chat_user = {}
+version = "v1"
 
 
 def get_chat_user(user_id: int) -> ChatUser:
@@ -22,6 +23,11 @@ def get_chat_user(user_id: int) -> ChatUser:
 def load_data():
     global chat_user
     chat_user = read_data()
+    if chat_user.get("__version__") != version:
+        write_data({
+            "__version__": version,
+        })
+        chat_user = read_data()
 
 
 @scheduler.scheduled_job("interval", seconds=300)
@@ -31,7 +37,9 @@ def save_data():
 
 def read_data() -> dict:
     if not store.get_data_file("chatgpt", "data.dat").exists():
-        write_data({})
+        write_data({
+            "__version__": version,
+        })
     with open(store.get_data_file("chatgpt", "data.dat"), "rb") as file:
         return pickle.load(file)
 
