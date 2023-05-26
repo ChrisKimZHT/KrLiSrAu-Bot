9a825523a1e7b67cd6e662d9499ec10818ba8d5a
ChrisKim <1515748548@qq.com>
2023-05-26 15:48:35 +0800

    [fix]: <chatgpt> 重置后运行新对话

--- chatgpt/__init__.py
+++ chatgpt/__init__.py
@@ -33,7 +33,7 @@ __plugin_meta__ = PluginMetadata(
     config=Config,
     extra={
         "authors": "ChrisKim",
-        "version": "2.4.0",
+        "version": "2.4.1",
         "KrLiSrAu-Bot": True,
     }
 )
@@ -84,9 +84,9 @@ async def _(matcher: Matcher, event: MessageEvent, state: T_State, is_reset: Mes
         preset_id = get_chat_user(user_id).get_instance().get_preset_id()
         res: Optional[ChatResult] = await get_chat_user(user_id).reset_instance(preset_id)
         if res is None:
-            await chatgpt_reset.finish("无预设重置对话完成")
+            await chatgpt_reset.send("无预设重置对话完成")
         else:
-            await chatgpt_reset.finish(f"使用预设 {preset_id} 重置对话完成：\n" + res.get_content_str())
+            await chatgpt_reset.send(f"使用预设 {preset_id} 重置对话完成：\n" + res.get_content_str())
 
     chat_inst: ChatInst = get_chat_user(user_id).get_instance()
     await chat(matcher, chat_inst, user_message)
